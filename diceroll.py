a2c4d8d56948f7f49e42ca4c7fafcc60e629464d
Anmol Anand <anmolkanand@yahoo.com>
2014-05-04 13:13:11 -0400

    Checking for valid numbers

--- diceroll.py
+++ diceroll.py
@@ -1,7 +1,20 @@
 #!/usr/bin/wpython
 import random 
 
-number = random.randint(1,6)
+def int_input(sentence):
+	while True:
+		number = raw_input(sentence)
+		try:
+			number = int(number)
+			if number <= 0:
+				print "Please enter a number greater than zero"
+			else:	
+				return number
+		except:
+			print "Not a number"
+			
+side = int_input("How many sides are on the dice? ")
+number = random.randint(1, side) 
 print "Here is the number the dice gave: %d" % (number)
 
 
